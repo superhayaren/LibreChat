e5169c23d6e9faba8f16c32dcc256aaa467c2976
SHI YU WANG <110075927+superhayaren@users.noreply.github.com>
2023-12-08 10:43:36 +0800

    部署问题

--- api/server/utils/crypto.js
+++ api/server/utils/crypto.js
@@ -4,6 +4,10 @@ const iv = Buffer.from(process.env.CREDS_IV, 'hex');
 const algorithm = 'aes-256-cbc';
 
 function encrypt(value) {
+  if (value === undefined) {
+    throw new Error('Value to encrypt must be provided');
+  }
+
   const cipher = crypto.createCipheriv(algorithm, key, iv);
   let encrypted = cipher.update(value, 'utf8', 'hex');
   encrypted += cipher.final('hex');
@@ -11,6 +15,10 @@ function encrypt(value) {
 }
 
 function decrypt(encryptedValue) {
+  if (encryptedValue === undefined) {
+    throw new Error('Encrypted value must be provided');
+  }
+
   const decipher = crypto.createDecipheriv(algorithm, key, iv);
   let decrypted = decipher.update(encryptedValue, 'hex', 'utf8');
   decrypted += decipher.final('utf8');
